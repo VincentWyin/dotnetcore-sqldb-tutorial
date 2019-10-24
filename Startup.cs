41eb8e10b6429762953d5d7bae196a5b9325bea5
vincentzhao <vincent.zhao@everbright165.com>
2019-10-24 17:52:08 +0800

    change the db to sql database (temp)

--- Startup.cs
+++ Startup.cs
@@ -34,18 +34,19 @@ namespace DotNetCoreSqlDb
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            
+
             // Use SQL Database if in Azure, otherwise, use SQLite
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-            {
-                services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
-            }
-            else
-            {
-                services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
-            }
+            //if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            //{
+            //}
+            //else
+            //{
+            //    services.AddDbContext<MyDatabaseContext>(options =>
+            //        options.UseSqlite("Data Source=localdatabase.db"));
+            //}
+
+            services.AddDbContext<MyDatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
 
             // Automatically perform database migration
             services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();
